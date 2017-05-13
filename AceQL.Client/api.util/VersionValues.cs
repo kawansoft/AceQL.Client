772687d64dd9c21ee80cf3e7e9c1b05e5435a95b
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2017-05-13 02:30:03 +0200

    Clean comments

--- AceQL.Client/api.util/VersionValues.cs
+++ AceQL.Client/api.util/VersionValues.cs
@@ -41,6 +41,7 @@
 //13/05/2017 00:27 NDP: Delete class FormUpload
 //13/05/2017 02:04 NDP: PortableFile entirely rewritten: no more hiding or IFile/IFolder
 //13/05/2017 02:28 NDP: License is now Apache 2.0
+//13/05/2017 02:29 NDP: Clean comments
 
 namespace AceQL.Client.Api.Util
 {
