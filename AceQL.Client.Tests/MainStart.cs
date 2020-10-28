39e31cb7ba0abe92d6f977c420475c5c1af86bef
Nicolas de Pomereu <ndepomereu@kawansoft.com>
2020-10-28 19:03:07 +0100

    MainStart update

--- AceQL.Client.Tests/MainStart.cs
+++ AceQL.Client.Tests/MainStart.cs
@@ -36,7 +36,7 @@ namespace AceQL.Client.Tests
 
         public static void Main(string[] args)
         {
-            int mainToLaunch = 5;
+            int mainToLaunch = 6;
 
             if (mainToLaunch == 1)
             {
